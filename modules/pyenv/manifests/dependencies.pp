40754a09006ae7f0e64521f9c5b294caef3a11d2
Xander Shepherd <n00b@n00bsys0p.co.uk>
2015-01-25 23:10:53 +0000

    Chrome image.

--- modules/pyenv/manifests/dependencies.pp
+++
@@ -1,9 +0,0 @@
-# Setup dependencies depends on os family
-#
-class pyenv::dependencies {
-  case $::osfamily {
-    debian         : { require pyenv::dependencies::debian    }
-    redhat         : { require pyenv::dependencies::redhat    }
-    default        : { notice("Could not load dependencies for ${::osfamily}") }
-  }
-}
